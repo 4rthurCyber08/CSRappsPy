fcd7a3655f34132780f156f239e22edb84a629f6
ArJames Inson <aerin.james820@gmail.com>
2025-06-06 17:52:38 +0800

    Update getShCommands.py

--- Lab1 - apps_csrJsonPy/script/getShCommands.py
+++ Lab1 - apps_csrJsonPy/script/getShCommands.py
@@ -16,27 +16,27 @@ set = jsonConfig['line_and_ip']
 
 accessCli = ConnectHandler(**device_info)
 sh_ip = accessCli.send_command('sh ip int br', use_textfsm=True)
-#print(type(sh_ip))
+# print(type(sh_ip))
 
-#create then open the json file
+# create then open the json file
 with open('sh_ip_int_br.json', 'w') as blankFile:
     
-    #convert the object type into str in order to write it into json
+    # convert the object type into str in order to write it into json
     content = json.dumps(sh_ip, indent=2)
-    #write the content into the json file
+    # write the content into the json file
     blankFile.write(content)
 
 
-##Experiment with python reading specefic lines of a json file##
-#read the json file and iterate through its contents
+## Experiment with python reading specific lines of a json file
+# read the json file and iterate through its contents
 with open('sh_ip_int_br.json', 'r') as readFile:
     
-    #make python read through the json file, then output as specific ip address: 192.168.102.5
+    # make python read through the json file, then output as specific ip address: 192.168.102.5
     readContent = json.load(readFile)
     for i in readContent:
         ints = i['ip_address']
         
-        #specify a string that ends with 2.5
+        # specify a string that ends with 2.5
         regex = r'2.5$'
         output = re.search(regex, ints)
         if output:
